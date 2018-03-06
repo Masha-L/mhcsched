f4783f8ed75fea70257bdccdfd9921161194c5bb
wshcksh <lis22s@mtholyoke.edu>
2018-03-06 15:42:21 -0500

    tp
    answers, comments

--- Schedule/src/Application/TimePeriod.java
+++ Schedule/src/Application/TimePeriod.java
@@ -1,26 +1,37 @@
 package Application;
+
+
+	// QUESTION: Do we want to do decimals for minutes?
+	// We can just make them hour.00-hour.59
+	//
+	// Answer: I think, 00-59 is enough.
+
+
 /**
- * 
- * @author Mashka
+ * Provides an abstraction for a single class. 
+ * Uses 24-hour format to hold the time (0.00 - 23.59) 
+ * Uses a short number to hold a day of the week (1 - Mon, 2 - Tue ... 7 - Sun).
  *
+ * @author Jack
+ * @version 2
+ * @date 3/6/18
  */
 public class TimePeriod {
+	// Start time of the class
+	private double startTime;
+	// End time of the class
+	private double endTime;
+	// Weekday of the class
+	private short weekDay;
 
-	//QUESTION: Do we want to do decimalsfor minutes?
-	//We can just make them hour.00-hour.59
 	/**
-	 * @param startTime and endTime double represent
-	 *  the start and end times of a class in 24 hour format
-	 *  Minutes are ...
-	 *  @param weekDay short 
-	 *  a number represents a day of the week
-	 *   1 - Mon, 2 - Tue ... 7 - Sun
-	 */
-	double startTime;
-	double endTime;
-	short weekDay;
-	
-	TimePeriod(double startTime,	double endTime,	short weekDay)
+	 * Constructs a Time Period.
+	 *
+	 * @param startTime the start time of the class
+	 * @param endTime the end time of the class 
+	 * @param weekDay the day of the week      
+	 */	
+	public TimePeriod(double startTime, double endTime, short weekDay)
 	{
 		this.startTime = startTime;
 		this.endTime = endTime;
