61f5f5f115fe2b79c6e8797d12235e6ee9f857ff
Mashka <Mashka@n234-93.mtholyoke.edu>
2018-03-06 12:58:38 -0500

    Time Period class

--- Schedule/src/Application/TimePeriod.java
+++ Schedule/src/Application/TimePeriod.java
@@ -1,5 +1,29 @@
 package Application;
-
+/**
+ * 
+ * @author Mashka
+ *
+ */
 public class TimePeriod {
 
+	//QUESTION: Do we want to do decimalsfor minutes?
+	//We can just make them hour.00-hour.59
+	/**
+	 * @param startTime and endTime double represent
+	 *  the start and end times of a class in 24 hour format
+	 *  Minutes are ...
+	 *  @param weekDay short 
+	 *  a number represents a day of the week
+	 *   1 - Mon, 2 - Tue ... 7 - Sun
+	 */
+	double startTime;
+	double endTime;
+	short weekDay;
+	
+	TimePeriod(double startTime,	double endTime,	short weekDay)
+	{
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.weekDay = weekDay;
+	}
 }
